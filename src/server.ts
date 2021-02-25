caf6092f57e2ea265415e97140499cc07ed5e290
gabriel paulo <gabriellemos11@hotmail.com>
2021-02-25 10:35:20 -0300

    Dia 02 - Banco de dados

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,13 @@
-import express from 'express'
+import 'reflect-metadata';
+import express from 'express';
+import './database';
+import { router } from './routes';
 
-const app = express()
+const app = express();
+
+app.use(express.json());
+app.use(router);
 
 app.listen(3000, () => {
-	console.log('Server is Running!')
+	console.log('Server is Running!');
 })
